ecfd08fb44214c39e01e33b7de1f2093689d2e08
Cristian Pacheco <cristian.pacheco@llactatec.com>
2020-03-06 10:14:20 -0500

    commit inicial

---
+++ src/layouts/LayoutAdmin.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Route} from 'react-router-dom';
+import {Layout} from "antd";
+
+import "./LayoutAdmin.scss";
+
+export default function LayoutAdmin(props) {
+
+    //console.log(props);
+
+    const { routes } = props;
+    const  {Header, Content, Footer} = Layout;
+    return(
+
+        <Layout>
+            <h2>Menu Sider Admin</h2>
+            <Layout>
+                <Header>Header</Header>
+                <Content>
+                    <LoadRouters routes={routes} />
+                </Content>
+                <Footer>
+                    crispac
+                </Footer>
+            </Layout>
+        </Layout>
+
+    )
+}
+
+function LoadRouters( {routes} ){
+
+    return routes.map((route, index) => (
+         <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+        />
+    ))
+}
---
+++ src/pages/Contacts.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contacts() {
+
+    return (
+        <div>
+            <h1>Estamos en Contacs</h1>
+        </div>
+    )
+}
